1a5ddedf9bb5933b1f0fe01c8c0f46bf6d9196df
safaleks <safaleks@mail.ru>
2022-09-09 22:53:09 +0300

    БонусМили

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate (int cost) {
+        return cost / 20;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 7860;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
 }
